b339f48c15314bd8c77ccc85e0d716f115107bd9
mohammad <mohi_6038550@yahoo.com>
2016-06-24 11:58:24 +0430

    rule correcting

--- twoscreen/src/sample/Rule.java
+++ twoscreen/src/sample/Rule.java
@@ -105,7 +105,7 @@ public class Rule {
 
         return false;
        /* if (RuleCompatibility(currentDec.wh))
-            return true;
+            return true;//
         else
             return false;*/
     }
